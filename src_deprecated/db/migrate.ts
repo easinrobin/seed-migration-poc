a7879f33d980de662774d6ee173692baa65428bb
easinrobin-tvs <easin.robin@technovativesolutions.co.uk>
2025-11-28 19:56:46 +0600

    remove unused code

--- src_deprecated/db/migrate.ts
+++
@@ -1,8 +0,0 @@
-import { migrate } from "drizzle-orm/node-postgres/migrator";
-import { db } from "../config/db";
-
-(async () => {
-  console.log("🚀 Running migrations...");
-  await migrate(db, { migrationsFolder: "./drizzle" });
-  console.log("✅ Migrations complete");
-})();
